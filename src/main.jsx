bb9f4d8a2bb00e680007485c2b5555c9ddda0752
siriwatknp <siriwatkunaporn@gmail.com>
2023-05-16 23:44:59 +0700

    set up msw

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { worker } from "./mocks/browser";
+
+async function prepare() {
+  if (import.meta.env.DEV) {
+    return worker.start();
+  }
+}
+
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
