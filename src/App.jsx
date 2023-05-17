66163b47600635a78e13a280598085299186f4c7
siriwatknp <siriwatkunaporn@gmail.com>
2023-05-17 14:02:16 +0700

    crud: able to remove a row

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ import {
   DataGrid,
   GridToolbarContainer,
   useGridApiContext,
+  GridActionsCellItem,
 } from "@mui/x-data-grid";
 import { unstable_joySlots as joySlots } from "@mui/x-data-grid/joy";
+import DeleteIcon from "@mui/icons-material/Delete";
 import countries from "./countries.json";
 
 const DATA = [
@@ -172,6 +174,23 @@ function App() {
             type: "date",
             // to learn more, visit: https://mui.com/x/react-data-grid/column-definition/#value-formatter
             valueFormatter: (params) => new Date(params.value).toDateString(),
+          },
+          {
+            field: "actions",
+            type: "actions",
+            getActions: (params) => [
+              <GridActionsCellItem
+                key="delete"
+                icon={<DeleteIcon />}
+                label="Delete"
+                onClick={() => {
+                  setRows((prevRows) =>
+                    prevRows.filter((item) => item.id !== params.row.id)
+                  );
+                }}
+                color="inherit"
+              />,
+            ],
           },
         ]}
         rows={rows}
