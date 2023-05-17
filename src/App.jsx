290c4ee50002df229688de367eeba845c8efe8a7
siriwatknp <siriwatkunaporn@gmail.com>
2023-05-17 13:01:24 +0700

    render a datagrid - ready to start!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import CssBaseline from "@mui/joy/CssBaseline";
+import Container from "@mui/joy/Container";
+import Typography from "@mui/joy/Typography";
+import { DataGrid } from "@mui/x-data-grid";
+
+const DATA = [
+  {
+    id: "1",
+    name: "spray",
+    manufacturedDate: new Date().toString(),
+    price: 200,
+  },
+  {
+    id: "2",
+    name: "foam",
+    manufacturedDate: new Date("2021-05-22").toString(),
+    price: 120,
+  },
+];
+
 function App() {
-  return <>Hello!</>;
+  return (
+    <Container>
+      <CssBaseline />
+      <Typography component="h1" level="h3" sx={{ my: 3 }}>
+        Joy DataGrid - CRUD Workshop
+      </Typography>
+      <DataGrid
+        columns={[
+          {
+            field: "id",
+            headerName: "ID",
+          },
+        ]}
+        rows={DATA}
+      />
+    </Container>
+  );
 }
 
 export default App;
